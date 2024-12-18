0049bdf3c1f1604ea4e6e0d85065b1f89d484a94
Marcos Vieco <159963869+MarcosVieco@users.noreply.github.com>
2024-12-18 10:37:22 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -9,24 +9,50 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    eye_color = Column(String(250))
+    gender = Column(String(250))
+    height = Column(Integer)   
+    mass = Column(Integer)
+    planet = Column(Integer, ForeignKey('planets.id'))
+    vehicle = Column(Integer, ForeignKey('vehicles.id'))
+    starship = Column(Integer, ForeignKey('starships.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False, unique=True)
+    climate = Column(String(250))
+    diameter = Column(Integer)
+    persons = relationship('Person', backref=('planet'))
 
-    def to_dict(self):
-        return {}
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250))
+    cost_in_credits = Column(Integer)
+    cargo_capacity = Column(Integer)
+    persons = relationship('Person', backref=('vehicle'))
+    
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250))
+    cost_in_credits = Column(Integer)
+    max_atmosphering_speed = Column(Integer)
+    hyperdrive_rating = Column(Integer)
+    persons = relationship('Person', backref=('starship'))
+
+class Favourites(Base):
+    __tablename__ = 'favourites'
+    id = Column(Integer, primary_key=True)
+    base_type = Column(String(250), nullable=False)
+    base_id = Column(Integer, nullable=False)
+git add 
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
